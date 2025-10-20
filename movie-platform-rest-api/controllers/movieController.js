ba0e6e6ef1609cddd06a78c91df307fb7e220b7d
Alfredo <alfredo.e.p5787@gmail.com>
2025-10-19 23:21:35 -0600

    removing unused promises functions and adapt just to async for each get request, up to now one to fetch all movies (with filters if any), and other to get movie by id

--- movie-platform-rest-api/controllers/movieController.js
+++ movie-platform-rest-api/controllers/movieController.js
@@ -3,7 +3,6 @@ const database = require('../config/database.cjs');
 //Helper function to parse main_actorsJSON
 const parseMainActors = (mainActors) =>{
     try{
-        console.log(mainActors);
         return mainActors ? mainActors.split(',').map(actor => actor.trim()) : [];
     }catch{
         return [];
@@ -15,35 +14,99 @@ const parseMainActors = (mainActors) =>{
 
 const getAllMovies = async (req, res)=>{
     const db = await database.connect();
-    await new Promise((resolve, reject)=>{
-        const {title, release_year, genere, synopsis, country, views, directed_by, main_actors} = req.query;
+        const {title, release_year, genere, country, directed_by, main_actors} = req.query;
         const params = [];
         let sql = `SELECT * FROM movies WHERE active_record=1`;
+
+        //Adding filters
+        if(title){
+            sql+=` AND titile LIKE ?`;
+            params.push(title);
+        }
+
+        if(release_year){
+            sql+=` AND release_year=?`;
+            params.push(release_year);
+        }
+
+        if(genere){
+            sql+=` AND genere=?`;
+            params.push(genere);
+        }
+
+        if(country){
+            console.log(country);
+            sql+=` AND country=?`;
+            params.push(country);
+        }
+
+        if(directed_by){
+            sql+=` AND directed_by LIKE ?`;
+                params.push(`%${directed_by}%`);            
+        }
+
+        if(main_actors){
+            sql+=` AND main_actors LIKE ?`;
+            params.push(`%${main_actors}%`);
+        }
         
         db.all(sql,params, (err, rows)=>{
             if(err){
                 return res.status(500).json({
                     success: false,
-                    error: 'Database Error'
+                    error: `Database Error: ${err}`
                 }); 
             }
 
-            const movies = rows.map(row=>({
-                ... row,
-                main_actors: parseMainActors(row.main_actors)
-            }));
+        const movies = rows.map(row=>({
+            ... row,
+            main_actors: parseMainActors(row.main_actors)
+        }));
 
-            res.json({
+        return res.json({
                 success: true,
                 count: movies.length,
                 data: movies
-            });
-        })
+        });
     });
 
     await database.disconnect();
 };
 
+const getMovie = async (req,res)=>{
+    const movieId = parseInt(req.params.id);
+    const db =  await database.connect();
+
+    db.get(`SELECT * FROM movies WHERE id = ? AND active_record=1`,[movieId], (err, row)=>{
+            if(err){
+                return res.status(500).json({
+                    success: false,
+                    error: `Database error: ${err}`
+                });
+            }
+            
+            if(!row){
+                return res.status(404).json({
+                    success: false,
+                    error: `Movie with id: ${movieId} was not found`
+                })
+            }
+
+            //Parse mainactors
+            const movie = {
+                ... row,
+                main_actors: parseMainActors(row.main_actors)
+            };
+
+            res.json({
+                success:true,
+                data: movie
+            });
+        });
+    await database.disconnect();
+};
+
 module.exports = {
-    getAllMovies
+    getAllMovies,
+    getMovie
 }
