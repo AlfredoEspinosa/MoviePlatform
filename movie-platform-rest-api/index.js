c049a6a6b1bfe2fa9578cd70e7bc4390fcdf6c30
Alfredo <alfredo.e.p5787@gmail.com>
2025-10-19 23:24:16 -0600

    Formatting

--- movie-platform-rest-api/index.js
+++ movie-platform-rest-api/index.js
@@ -8,19 +8,19 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/api/movies', movieRoutes);
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.json({
         message: 'Node.js Server 💻',
-        endpoints:{
-          'GET /api/movies': 'Get all movies ',
+        endpoints: {
+            'GET /api/movies': 'Get all movies (filters: title, release_year, genere, country, directed_by, main_actors)',
         }
     });
 });
 
- // Error handling middleware
+// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is up and running and listening in port ${PORT}`);
 })
